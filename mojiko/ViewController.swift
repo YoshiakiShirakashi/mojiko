6301e53bfb28e9d5982421e6f2520b60d31647a2
YoshiakiShirakashi <mikan014140@yahoo.co.jp>
2019-08-13 20:09:36 +0900

    Initial Commit

---
+++ mojiko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mojiko
+//
+//  Created by 白樫芳昭 on 2019/08/13.
+//  Copyright © 2019 yoshiaki.sjirakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
